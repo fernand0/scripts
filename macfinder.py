c244e0ccbf031d34e99ce010139956469737392f
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2014-07-11 00:05:40 +0200

    Added persistence, storing a file with data and reading it

--- macfinder.py
+++ macfinder.py
@@ -34,6 +34,18 @@ except:
     print("Unexpected error:", sys.exc_info()[0])
     sys.exit(0)
 
+def loadData():
+    try:
+	fIP = open("ipList.txt","r")
+        try:
+            ipList=pickle.load(fIP)
+        except:
+            print "The file does not contain adequate data\n"
+    except: 
+        print "No file with stored ips \n" 
+    print ipList
+    return ipList
+
 def seek():                        # defines a function to analize the network
     count = 0
     nm.scan(hosts='192.168.1.0/24', arguments='-n -sP -PE -T5')
@@ -46,7 +58,7 @@ def seek():                        # defines a function to analize the network
         count = count + 1
 	try:
 		if not ipList.has_key(addresses['mac']):
-			ipList[addresses['mac']] = addresses['ipv4']
+			ipList[addresses['mac']] = ("", addresses['ipv4'])
 		
 	except:
 		pass
@@ -55,13 +67,16 @@ def seek():                        # defines a function to analize the network
 
 def name():                        # defines a function to analize the network
     for mac in ipList.keys():
-	name = raw_input(mac+" Name? ")
+	name = raw_input(mac+"("+ipList[mac][0]+", "+ipList[mac][1]+") Name? ")
 	print mac, name
-	ipList[mac]=(name, ipList[mac])
+        if name != "":
+	    ipList[mac]=(name, ipList[mac])
     
 if __name__ == '__main__':
     count = 1
 
+    ipList=loadData()
+    print ipList
     # check if the number of addresses is still the same
     while (count <= 10):
         print "Pass: ",count, "Found: ", seek(), "Total: ", len(ipList)
