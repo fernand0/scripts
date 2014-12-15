172a51873eb47db85481100cb761338aa0c2ac9c
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2014-12-15 23:22:12 +0100

    First commit for rssToPages.py

---
+++ rssToPages.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import ConfigParser, os, re
+import facebook
+import pprint
+import feedparser
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+from bs4 import Tag
+
+
+
+config = ConfigParser.ConfigParser()
+
+config.read([os.path.expanduser('~/.rssBlogs')])
+
+print "Configured blogs:"
+
+i=1
+for section in config.sections():
+	print str(i), ')', section, config.get(section, "rssFeed")
+	i = i + 1
+
+if (int(i)>1):
+	i = raw_input ('Select one: ')
+else:
+	i = 1
+
+print "You have chosen ", config.get("Blog"+str(i), "rssFeed")
+
+
+
+rssFeed = config.get("Blog"+str(i), "rssFeed")
+pageFB = config.get("Blog"+str(i), "pageFB")
+
+
+print "*"+rssFeed+"*"
+
+feed = feedparser.parse(rssFeed)
+
+print feed
+
+i = 0
+
+
+
+soup = BeautifulSoup(feed.entries[i].title)
+theTitle = soup.get_text()
+theLink =  feed.entries[i].link
+
+soup = BeautifulSoup(feed.entries[i].summary)
+
+# Now the links
+
+j = 0
+linksTxt = ""
+for link in soup("a"):
+	# print type(link.contents[0])
+	if not isinstance(link.contents[0], Tag):
+		# We want to avoid embdeded tags (mainly <img ... )
+		if not re.search(r"delicious.*fernand0.*rei", link['href']):
+			# We want to avoid tags in fernand0.blogalia.com
+			link.append(" ["+str(j)+"]")
+			j =  j + 1
+			linksTxt = linksTxt + "["+str(j)+"] " + link.contents[0] + "\n"
+			linksTxt = linksTxt + "    " + link['href'] + "\n"
+
+theSummary = theTitle+"\n"
+theSummary = theSummary + soup.get_text()
+theSummary = theSummary+"\n\n"
+
+if linksTxt != "":
+	theSummary = theSummary + linksTxt
+theSummary = theSummary+"\n\n"
+
+print theSummary
+
+
+config.read([os.path.expanduser('~/.rssFacebook')])
+oauth_access_token= config.get("Facebook", "oauth_access_token")
+
+graph = facebook.GraphAPI(oauth_access_token)
+pages = graph.get_connections("me", "accounts")
+
+for i in range(len(pages['data'])):
+	if (pages['data'][i]['name'] == pageFB):
+		print "Writing in... ", pages['data'][i]['name']
+		graph2 = facebook.GraphAPI(pages['data'][i]['access_token'])
+		graph2.put_object(pages['data'][i]['id'], 
+			"feed", message = 'Nueva entrada:\n\n'+theSummary, 
+			link=theLink, 
+			#picture = 'http://fernand0.github.io/', 
+			# You could select a picture
+			name=theTitle, caption='', description=theSummary)
+		
