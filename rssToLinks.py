a7e0ea78b5090766c214ac240064c60e27947d11
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2015-01-11 17:51:30 +0100

    Cleaning a bit the code

--- rssToLinks.py
+++ rssToLinks.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 
 # 
-# This program reads a feed an extracts the entries and the links they contain
+# This program reads a feed an extracts its entries and the links they contain
 # The idea is to have for each entry a list of the links at the end.
-# Each entry contains numbers in [x] and the number refers to the corresponding
-# link at the end. It has no HTML markup.
-# 
+# Each entry contains the text and a list of links. In the text there are
+# numbers in [x] for each link that reference the corresponding link at the
+# end. It has no HTML markup.
 
 import feedparser, re
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 from bs4 import Tag
 
-url='https://github.com/fernand0.atom'
-url='http://fernand0.tumblr.com/rss'
+url='https://fernand0.github.io/feed.xml' # Put your RSS feed here
 
 feed = feedparser.parse(url)
 
@@ -23,19 +22,21 @@ for i in range(len(feed.entries)):
 	
 	j = 0
 	linksTxt = ""
-	for link in soup("a"):
-		# print type(link.contents[0])
+	for link in links:
 		if not isinstance(link.contents[0], Tag):
 			# We want to avoid embdeded tags (mainly <img ... )
-			if not re.search(r"delicious.*fernand0.*rei", link['href']):
-				# We want to avoid tags in fernand0.blogalia.com
-				link.append(" ["+str(j)+"]")
-				j =  j + 1
-				linksTxt = linksTxt + "["+str(j)+"] " + link.contents[0] + "\n"
-				linksTxt = linksTxt + "    " + link['href'] + "\n"
+			link.append(" ["+str(j)+"]")
+			j =  j + 1
+			linksTxt = linksTxt 
+				+ "["+str(j)+"] " 
+				+ link.contents[0] + "\n"
+			linksTxt = linksTxt 
+				+ "    " 
+				+ link['href'] + "\n"
 	print "Entry "+str(i)+":"
 	print soup.get_text()
 	if linksTxt != "":
+		print
 		print "Links :"
 		print linksTxt
 	print
