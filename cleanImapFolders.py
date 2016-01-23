9bafdfaa8a25abbd76cdafcd6ea5204f49728f0c
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2016-01-24 00:03:45 +0100

    Grouping rules for accounts in order to avoid concurrency problems

--- cleanImapFolders.py
+++ cleanImapFolders.py
@@ -84,63 +84,66 @@ def getPassword(server, user):
     return password
 
 
-def mailFolder(server, user, password, rules, folder, logging):
-    SERVER = server
-    USER = user
-    PASSWORD = password
-    RULES = rules
-    FOLDER = folder
+def mailFolder(account, accountData, logging):
+    SERVER = account[0]
+    USER = account[1]
+    PASSWORD = accountData[0]
 
     M = imaplib.IMAP4_SSL(SERVER)
     M.login(USER, PASSWORD)
     password = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     # We do not want passwords in memory when not needed
     M.select()
-    i = 0
-    msgs = ''
-    for rule in RULES:
-        action = rule.split(',')
-        header = action[0][1:-1]
-        content = action[1][1:-1]
-        logging.info("[%s,%s] Rule: %s %s" % (SERVER, USER, header, content))
-        if (header == 'hash'):
-            msgs = selectHash(M, folder, content)
-            # M.select(folder)
-            FOLDER = ""
-        else:
-            typ, data = M.search(None, 'header', header, content)
-            if data[0]:
-                if msgs:
-                    msgs = msgs + ' ' + data[0]
-                else:
-                    msgs = data[0]
+
+    for actions in accountData[1]:
+        RULES = actions[0]
+        FOLDER = actions[1]
+
+        i = 0
+        msgs = ''
+        for rule in RULES:
+            action = rule.split(',')
+            header = action[0][1:-1]
+            content = action[1][1:-1]
+            logging.info("[%s,%s] Rule: %s %s" % (SERVER, USER, header, content))
+            if (header == 'hash'):
+                msgs = selectHash(M, FOLDER, content)
+                # M.select(folder)
+                FOLDER = ""
             else:
-                logging.info("[%s,%s] No messages matching" % (SERVER, USER))
+                typ, data = M.search(None, 'header', header, content)
+                if data[0]:
+                    if msgs:
+                        msgs = msgs + ' ' + data[0]
+                    else:
+                        msgs = data[0]
+                else:
+                    logging.info("[%s,%s] No messages matching" % (SERVER, USER))
 
-    if len(msgs)==0:
-        logging.info("[%s,%s] Nothing to do" % (SERVER, USER))
-    # elif not msgs[0]:
-    #    print "["+SERVER+","+USER+"]"+" -> Nothing to do (len 0, empty)"
-    else:
-        logging.info("[%s,%s] Let's go!" % (SERVER, USER))
-        msgs = msgs.replace(" ", ",")
-        status = 'OK'
-        if FOLDER:
-	    # M.copy needs a set of comma-separated mesages, we have a list
-	    # with a string
-            result = M.copy(msgs, FOLDER)
-            status = result[0]
-        i = msgs.count(',') + 1
-        logging.info("[%s,%s] *%s* Status: %s"% (SERVER,USER,msgs,status))
-        # And this?
-	# Maybe messages are 'disappearing' while we are working?
-	# hint:anti-spam Is it possible to lock the folder in order to avoid
-	# this? Can be dangerous (losing messages)?
+        if len(msgs)==0:
+            logging.info("[%s,%s] Nothing to do" % (SERVER, USER))
+        # elif not msgs[0]:
+        #    print "["+SERVER+","+USER+"]"+" -> Nothing to do (len 0, empty)"
+        else:
+            logging.info("[%s,%s] Let's go!" % (SERVER, USER))
+            msgs = msgs.replace(" ", ",")
+            status = 'OK'
+            if FOLDER:
+	        # M.copy needs a set of comma-separated mesages, we have a list
+	        # with a string
+                result = M.copy(msgs, FOLDER)
+                status = result[0]
+            i = msgs.count(',') + 1
+            logging.info("[%s,%s] *%s* Status: %s"% (SERVER,USER,msgs,status))
+            # And this?
+	    # Maybe messages are 'disappearing' while we are working?
+	    # hint:anti-spam Is it possible to lock the folder in order to avoid
+	    # this? Can be dangerous (losing messages)?
 
-        if status == 'OK':
-            # If the list of messages is too long it won't work
-            flag = '\\Deleted'
-            result = M.store(msgs, '+FLAGS', flag)
+            if status == 'OK':
+                # If the list of messages is too long it won't work
+                flag = '\\Deleted'
+                result = M.store(msgs, '+FLAGS', flag)
 #Traceback (most recent call last):
 #  File "/home/ftricas/.pyenv/versions/2.7.10/lib/python2.7/threading.py", line 810, in __bootstrap_inner
 #    self.run()
@@ -156,15 +159,15 @@ def mailFolder(server, user, password, rules, folder, logging):
 #    raise self.error('%s command error: %s %s' % (name, typ, data))
 #error: STORE command error: BAD ['Error in IMAP command STORE: Invalid messageset']
 
-            if result[0] == 'OK':
-                logging.info("[%s,%s] SERVER %s: %d messages have been deleted."
-                              % (SERVER, USER, SERVER, i))
+                if result[0] == 'OK':
+                    logging.info("[%s,%s] SERVER %s: %d messages have been deleted."
+                                  % (SERVER, USER, SERVER, i))
+                else:
+                    logging.info("[%s,%s] Couldn't delete messages!" 
+                                 % (SERVER, USER))
             else:
-                logging.info("[%s,%s] Couldn't delete messages!" 
-                             % (SERVER, USER))
-        else:
-            logging.info("[%s,%s] Couldn't move messages!" 
-                             % (SERVER, USER))
+                logging.info("[%s,%s] Couldn't move messages!" 
+                                 % (SERVER, USER))
     M.close()
     M.logout()
 
@@ -174,7 +177,6 @@ def main():
     config.read([os.path.expanduser('~/.IMAP.cfg')])
     logging.basicConfig(#filename='example.log',
                         level=logging.INFO,format='%(asctime)s %(message)s')
-
 
     threads = []
     i = 0
@@ -193,30 +195,36 @@ def main():
             FOLDER = ""
 
         logging.info("[%s,%s] Reading config" % (SERVER, USER))
-        if USER not in accounts:
+
+	# We are grouping accounts in order to avoid interference among rules
+	# on the same account 
+	if (SERVER, USER) not in accounts:
             PASSWORD = getPassword(SERVER, USER)
-            accounts[USER] = PASSWORD
+            accounts[(SERVER, USER)] = [PASSWORD, [] ]
+            accounts[(SERVER, USER)][1].append((RULES, FOLDER))
         else:
-            PASSWORD = accounts[USER]
+            accounts[(SERVER, USER)][1].append((RULES, FOLDER))
             logging.info("[%s,%s] Known password!" % (SERVER, USER))
 
-        # t = threading.Thread(target=mailFolder,
-        #                     args=(SERVER, USER, PASSWORD, RULES, FOLDER, logging))
+    for account in accounts.keys():
+        t = threading.Thread(target=mailFolder,
+                             args=(account, accounts[account], logging))
 	# We are using sequential code because when there are several set of
 	# rules for a folder concurrency causes problems
-	mailFolder(SERVER, USER, PASSWORD, RULES, FOLDER, logging)
+        # Hopefully solved
+	# mailFolder(account, accounts[account], logging)
         PASSWORD = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         # We do not want passwords in memory when not needed
-        # threads.append(t)
+        threads.append(t)
         i = i + 1
-    for user in accounts.keys():
-        accounts[user] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    #for user in accounts.keys():
+    #    accounts[user] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
-    #for t in threads:
-    #    t.start()
+    for t in threads:
+        t.start()
 
-    #for t in threads:
-    #    t.join()
+    for t in threads:
+        t.join()
 
     logging.info("%s The end!" % sys.argv[0])
 
