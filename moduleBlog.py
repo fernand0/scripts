f011b4f02dfd71c0d8a1beba038b540422f45f6c
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2017-11-18 18:44:49 +0100

    Adding xmlrpc interface

--- moduleBlog.py
+++ moduleBlog.py
@@ -1,5 +1,5 @@
-
 import configparser
+import xmlrpc.client
 import os
 import time
 import urllib
@@ -10,13 +10,23 @@ from bs4 import Tag
 class moduleBlog():
 
     def __init__(self):
+         self.url = ""
          self.rssFeed = ''
+         self.Id
          self.socialNetworks = {}
          self.linksToAvoid = ""
-         self.posts = None
+         self.postsRss = None
+         self.postsXmlrpc = None
          self.time = []
          self.buffer = None
+         self.xmlrpc = ''
  
+    def getUrl(self):
+        return(self.url)
+
+    def setUrl(self, url):
+        self.url = url
+
     def getRssFeed(self):
         return(self.rssFeed)
 
@@ -47,19 +57,54 @@ class moduleBlog():
     def setBufferapp(self, bufferapp):
         self.bufferapp = bufferapp
 
-    def getPosts(self):
-        return(self.posts)
- 
-    def setPosts(self, posts):
-        self.posts = posts
+    def getXmlRpc(self):
+        return(self.rssFeed)
 
-    def getBlogPosts(self):
-        self.setPosts(feedparser.parse(self.rssFeed))
+    def setXmlRpc(self, xmlrpc):
+        self.xmlrpc = xmlrpc
+
+    def getPostsRss(self):
+        return(self.postsRss)
+ 
+    def setPostsRss(self, posts):
+        self.postsRss = posts
+
+    def getPostsXmlrpc(self):
+        conf = configparser.ConfigParser() 
+        conf.read('/home/ftricass/.blogaliarc') 
+        for section in conf.sections(): 
+            usr = conf.get(section,'login') 
+            pwd = conf.get(section,'password') 
+            srv = conf.get(section,'server')
+            if (self.url in srv):
+                self.setId(blogId(srv, usr, pws))
+
+        return(self.postsRss)
+ 
+    def setPostsXmlrpc(self, posts):
+        self.postsRss = posts
+
+    def getId(self):
+        return(self.Id)
+
+    def setId(self, Id):
+        self.Id = Id
+
+    def getBlogPostsRss(self):
+        self.setPostsRss(feedparser.parse(self.url+self.rssFeed))
+
+    def blogId(self, srv, usr, pwd): 
+        server = xmlrpc.client.ServerProxy(srv)
+        listMet = server.system.listMethods() 
+        if 'wp' in listMet[-1]:
+            userBlogs = server.wp.getUsersBlogs(usr,pwd)
+        else: 
+            server.blogger.getUsersBlogs('',usr,pwd)
 
     def getLinkPosition(self, link):
         i = 0
-        if self.getPosts():
-            for entry in self.getPosts().entries:
+        if self.getPostsRss():
+            for entry in self.getPostsRss().entries:
                 lenCmp = min(len(entry['link']), len(link))
                 if entry['link'][:lenCmp] == link[:lenCmp]:
                        return i
@@ -67,10 +112,10 @@ class moduleBlog():
         return(-1)
 
     def datePost(self, pos):
-        return(self.getPosts().entries[pos]['published_parsed'])
+        return(self.getPostsRss().entries[pos]['published_parsed'])
 
     def checkLastLink(self):
-        rssFeed = self.getRssFeed()
+        rssFeed = self.getUrl()+self.getRssFeed()
         urlFile = open(os.path.expanduser("~" + "/."  
                   + urllib.parse.urlparse(rssFeed).netloc
                   + ".last"), "r")
@@ -125,7 +170,7 @@ class moduleBlog():
         return theSummaryLinks
 
     def obtainPostData(self, i):
-        posts = self.getPosts().entries
+        posts = self.getPostsRss().entries
         theSummary = posts[i]['summary']
         content = posts[i]['description']
         theDescription = posts[i]['description']
@@ -200,6 +245,8 @@ if __name__ == "__main__":
         rssFeed = config.get(section, "rssFeed")
         print(rssFeed)
         blog = moduleBlog.moduleBlog()
+        url = config.get(section, "url")
+        blog.setUrl(url)
         blog.setRssFeed(rssFeed)
         optFields = ["linksToAvoid", "time", "bufferapp"]
         if ("linksToAvoid" in config.options(section)):
@@ -208,30 +255,33 @@ if __name__ == "__main__":
             blog.setTime(config.get(section, "time"))
         if ("bufferapp" in config.options(section)):
             blog.setBufferapp(config.get(section, "bufferapp"))
+        if ("xmlrpc" in config.options(section)):
+            blog.setXmlRpc(config.get(section, "xmlrpc"))
 
         for option in config.options(section):
             if ('ac' in option) or ('fb' in option):
                 blog.addSocialNetwork((option, config.get(section, option)))
-        blog.getBlogPosts()
+        blog.getBlogPostsRss()
         blogs.append(blog)
 
     for blog in blogs:
+        print(blog.getUrl())
         print(blog.getRssFeed())
         print(blog.getSocialNetworks())
         if 'twitterac' in blog.getSocialNetworks():
             print(blog.getSocialNetworks()['twitterac'])
-        print(blog.getPosts().entries[0]['link'])
-        print(blog.getLinkPosition(blog.getPosts().entries[0]['link']))
+        print(blog.getPostsRss().entries[0]['link'])
+        print(blog.getLinkPosition(blog.getPostsRss().entries[0]['link']))
         print(time.asctime(blog.datePost(0)))
-        print(blog.getLinkPosition(blog.getPosts().entries[5]['link']))
+        print(blog.getLinkPosition(blog.getPostsRss().entries[5]['link']))
         print(time.asctime(blog.datePost(5)))
         blog.obtainPostData(0)
 
     for blog in blogs:
         import urllib
         urlFile = open(os.path.expanduser("~" + "/."  
-              + urllib.parse.urlparse(blog.getRssFeed()).netloc
+              + urllib.parse.urlparse(blog.getUrl()+blog.getRssFeed()).netloc
               + ".last"), "r")
         linkLast = urlFile.read().rstrip()  # Last published
-        print(blog.getRssFeed(),blog.getLinkPosition(linkLast))
-        print("description ->", blog.getPosts().entries[5]['description'])
+        print(blog.getUrl()+blog.getRssFeed(),blog.getLinkPosition(linkLast))
+        print("description ->", blog.getPostsRss().entries[5]['description'])
