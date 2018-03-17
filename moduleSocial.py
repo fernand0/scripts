1e0b993778f8c27e64f9eeae6df079d4791cf0f3
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2018-03-17 21:16:44 +0100

    Adapting publishDelayFacebook

--- moduleSocial.py
+++ moduleSocial.py
@@ -369,7 +369,7 @@ def publishDelayFacebook(listPosts, fbPage, timeSlots):
         time.sleep(tSleep) 
         print("I'd publish ... %s" % str(listPosts[j])) 
         (title, link, firstLink, image, summary, summaryHtml, summaryLinks, comment) = listPosts[j - 1]
-        publishTwitter(title, firstLink, '', '', fbPage)
+        publishFacebook(fbPage, title, firstLink, summary, summaryHtml, summaryLinks, image)
         print("Time: %s Waiting ... %s" % (time.asctime(), str(tSleep2)))
         time.sleep(tSleep2) 
 
