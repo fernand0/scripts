e4f67b249bfe742489667fad957bb4739e73227f
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2018-09-24 18:38:22 +0200

    Now we do not need to encode strings ¿?

--- moduleSocial.py
+++ moduleSocial.py
@@ -370,9 +370,9 @@ def publishBuffer(blog, profile, title, link, firstLink, isDebug, lenMax, servic
         # Without the http or https 
         try:
             if titlePostT and (profile['service'] == 'twitter'):
-                entry = urllib.parse.quote(titlePostT + " " + firstLink).encode('utf-8')
+                entry = urllib.parse.quote(titlePostT + " " + firstLink)#.encode('utf-8')
             else:
-                entry = urllib.parse.quote(post).encode('utf-8')
+                entry = urllib.parse.quote(post)#.encode('utf-8')
 
             if (profile['service'][0] in services): 
                 profile.updates.new(entry)
