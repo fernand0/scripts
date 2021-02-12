8bdb9ed50161e593a05922ca26057c6ea3c382d0
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2021-02-12 10:48:08 +0100

    the script did not work well, it was missing the username field from keepass

--- keepassToPass.sh
+++ keepassToPass.sh
@@ -8,8 +8,8 @@ fi
 
 while IFS=, read -r f1 f2 f3 f4 f5 f6 f7 f8 ; 
 do   
-	echo "Creating pass entry for $f1"; 
-	echo "$f4" | pass insert -e "$f1" 
+	echo "Creating pass entry for $f1 $f2"; 
+	echo "$f4" | pass insert -e "$f1/$f2" 
 	sleep 1
 
 
